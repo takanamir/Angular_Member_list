8949c0e4d95bcdd819c3f2b03edc6748400d3a07
takanamir <rtakanami0706@gmail.com>
2020-07-15 17:54:28 +0900

    ダッシュボードを追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,45 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import { DashboardComponent } from './dashboard.component';
+import { MembersComponent }  from './members.component';
 import { MemberDetailComponent } from './member-detail.component';
+import { MemberService } from './member.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, MemberDetailComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot ([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'members',
+        component: MembersComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    MembersComponent,
+    MemberDetailComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ],
+  providers:[
+    MemberService
+  ]
 })
 export class AppModule { }
