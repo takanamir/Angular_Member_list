b142342ca39a80fe606ab72a61a0480e9d6c9970
takanamir <rtakanami0706@gmail.com>
2020-07-16 11:10:26 +0900

    ルーティングに応じたリンク設定・スタイル調整

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { DashboardComponent } from './dashboard.component';
@@ -9,25 +8,13 @@ import { MembersComponent }  from './members.component';
 import { MemberDetailComponent } from './member-detail.component';
 import { MemberService } from './member.service';
 
+import { AppRoutingModule } from './app-routing.module';
+
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot ([
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'members',
-        component: MembersComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      }
-    ])
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
